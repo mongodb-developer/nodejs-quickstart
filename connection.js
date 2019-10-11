b222f09d5b25552b302ec65995af3901eabb909c
Lauren Schaefer <lauren.schaefer@mongodb.com>
2019-10-11 12:03:51 -0400

    Update listDatabases

--- connection.js
+++ connection.js
@@ -6,7 +6,7 @@ async function main(){
      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
      */
     const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
-  
+
     /**
      * The Mongo Client you will use to interact with your database
      * See https://mongodb.github.io/node-mongodb-native/3.3/api/MongoClient.html for more details
@@ -32,10 +32,10 @@ main().catch(console.err);
 
 /**
  * Print the names of all available databases
- * @param {MongoClient} client A MongoClient that is connected to a cluster with the sample_airbnb database
+ * @param {MongoClient} client A MongoClient that is connected to a cluster
  */
 async function listDatabases(client){
-    databases = await client.db("sample_airbnb").admin().listDatabases();
+    databases = await client.db().admin().listDatabases();
 
     console.log("Databases:");
     databases.databases.forEach(db => console.log(` - ${db.name}`));
