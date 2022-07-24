dc39c5278b14e9d3af5fc5f7aa4ac9ebe9197cc9
Nayeem Ahmad Dar <Nayeemdar3@gmail.com>
2022-07-24 12:00:52 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -38,8 +38,8 @@ main().catch(console.error);
  * @param {MongoClient} client A MongoClient that is connected to a cluster
  */
 async function listDatabases(client) {
-    databasesList = await client.db().admin().listDatabases();
+ const   databasesList = await client.db().admin().listDatabases();
 
     console.log("Databases:");
     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
+}
